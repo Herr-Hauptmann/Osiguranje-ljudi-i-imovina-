a9d914efd111ef378b5efd3eef9a8ea7d6bf8739
Herr-Hauptmann <tarik.horoz@gmail.com>
2021-11-04 17:49:42 +0100

    broj posjetioca - done

---
+++ public/js/brojac.js
@@ -0,0 +1,13 @@
+var n = localStorage.getItem('on_load_counter');
+
+if (n === null) {
+    n = 0;
+}
+n++;
+
+localStorage.setItem("on_load_counter", n);
+nums = n.toString().split('').map(Number);
+document.getElementById('brojac_posjetioca').innerHTML = '';
+for (var i of nums) {
+    document.getElementById('brojac_posjetioca').innerHTML += '<span class="counter-item">' + i + '</span>';
+}
